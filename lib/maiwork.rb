f37578e71fdf6584efe48e29094fa008cbefa5f0
YujiAsakawa <leverages001+github@mediba.jp>
2014-07-04 15:46:52 +0900

    room id と api token を別ファイルにして、 Gmail 処理部分を削除した。

--- lib/maiwork.rb
+++ lib/maiwork.rb
@@ -2,10 +2,10 @@
 require 'faraday'
 require 'mechanize'
 
-class Chat
-  ROOM_ID = 'room_id' # room id
-  CHATWORK_TOKEN = 'apitoken' # api token
+$LOAD_PATH.push(File.expand_path(File.dirname(__FILE__)))
+require 'maiwork_config'
 
+class Chat
   def initialize
     @conn = Faraday::Connection.new(url: 'https://api.chatwork.com') do |builder|
       builder.use Faraday::Request::UrlEncoded
@@ -26,24 +26,4 @@ class Chat
 end
 
 chat = Chat.new
-
-agent = Mechanize.new
-agent.user_agent_alias = 'Mac Firefox'
-page = agent.get('https://accounts.google.com/')
-sso_page = page.form_with(id: 'gaia_loginform') do |form|
-  form.Email = 'emailaddress' # email address
-end.submit
-acs_page = sso_page.form_with(name: 'flogin') do |form|
-  form.uid = 'userid' # user id
-  form.password = 'password' # pass word
-end.submit
-redirect_page = acs_page.form_with(name: 'acsForm') do |form|
-end.submit
-redirect_page.link_with(text: 'Continue').click
-gmail_page = agent.get('https://mail.google.com/mail/u/0/?shva=1#inbox')
-gmail_page.body.each_line do |line|
-  case line.force_encoding('utf-8')
-  when /(\[Bigman\(production\)\]\[ERROR\] .+?)\\.+?(\d+年\d+月\d+日 \d+:\d+)/
-    chat.message = "#{$1} #{$2}"
-  end
-end
+chat.message = 'Ruby スクリプトからの api 投稿'
