671b8de303e2f3e8f29c8ca99dc1dfed18d56b54
YujiAsakawa <leverages001+github@mediba.jp>
2014-07-04 18:31:35 +0900

    github の api 操作クラス追加

--- lib/maiwork.rb
+++ lib/maiwork.rb
@@ -2,6 +2,7 @@
 require 'faraday'
 require 'mechanize'
 require 'json'
+require 'pp'
 
 $LOAD_PATH.push(File.expand_path(File.dirname(__FILE__)))
 require 'maiwork_config'
@@ -46,6 +47,41 @@ class Chat
   end
 end
 
-chat = Chat.new
-chat.message = 'api でメッセージ追加'
-chat.task = 'api でタスク追加'
+class GitHub
+  def initialize
+    @conn = Faraday::Connection.new(url: 'https://api.github.com') do |builder|
+      builder.use Faraday::Request::UrlEncoded
+      builder.use Faraday::Response::Logger
+      builder.use Faraday::Adapter::NetHttp
+    end
+  end
+
+  def commits
+    result = @conn.get do |request|
+      request.url "/repos/#{GITHUB_USER}/#{GITHUB_REPO}/commits'"
+      request.headers = {
+        'Authorization' => "token #{GITHUB_TOKEN}"
+      }
+    end
+    JSON.parse result.body
+  end
+
+  def issues(id = nil, comment = false)
+    result = @conn.get do |request|
+      request.url "/repos/#{GITHUB_USER}/#{GITHUB_REPO}/issues#{id ? "/#{id}" : ''}#{comment ? "/comments" : ''}"
+      request.headers = {
+        'Authorization' => "token #{GITHUB_TOKEN}"
+      }
+    end
+    JSON.parse result.body
+  end
+end
+
+# chat = Chat.new
+# chat.message = 'api でメッセージ追加'
+# chat.task = 'api でタスク追加'
+
+github = GitHub.new
+pp github.issues(777, true)[3]
+
+
