917a8a5cefe1f6b16ff7940e91aca53fef3ca183
YujiAsakawa <leverages001+github@mediba.jp>
2014-07-04 16:40:56 +0900

    initialize で room_id と account_id を取得するようにした

--- lib/maiwork.rb
+++ lib/maiwork.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 require 'faraday'
 require 'mechanize'
+require 'json'
 
 $LOAD_PATH.push(File.expand_path(File.dirname(__FILE__)))
 require 'maiwork_config'
@@ -12,11 +13,20 @@ class Chat
       builder.use Faraday::Response::Logger
       builder.use Faraday::Adapter::NetHttp
     end
+    result = @conn.get do |request|
+      request.url '/v1/me'
+      request.headers = {
+        'X-ChatWorkToken' => CHATWORK_TOKEN
+      }
+    end
+    me = JSON.parse result.body
+    @account_id = me['account_id']
+    @room_id = me['room_id']
   end
 
   def message=(text)
     @conn.post do |request|
-      request.url "/v1/rooms/#{ROOM_ID}/messages"
+      request.url "/v1/rooms/#{@room_id}/messages"
       request.headers = {
         'X-ChatWorkToken' => CHATWORK_TOKEN
       }
